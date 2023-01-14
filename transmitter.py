010bd6067bc929549c1c3e429b58a3124cb1c3ad
João Paulo Just Peixoto <just1982@gmail.com>
2023-01-14 19:02:31 +0000

    Make transmitter use Raspberry Pi camera

--- transmitter.py
+++ transmitter.py
@@ -1,15 +1,29 @@
 from socket import *
+from picamera import PiCamera
+import time
+
+SHOOTING_INTERVAL = 10
+TMP_IMG = 'img.jpg'
+PACKET_SIZE = 4096
+RECEIVER_ADDR = '127.0.0.1'
+RECEIVER_PORT = 12345
 
 if __name__ == '__main__':
     sock = socket(family=AF_INET, type=SOCK_DGRAM)
+    camera = PiCamera()
 
-    fp = open('example.jpg', 'rb')
+    while True:  # Camera loop
+        time.sleep(SHOOTING_INTERVAL)
+        camera.capture(TMP_IMG)
+        fp = open(TMP_IMG, 'rb')
 
-    while True:
-        data = fp.read(4096)
-        if len(data) == 0:
-            break
-        sock.sendto(data, ('127.0.0.1', 12345))
-    
-    fp.close()
-    sock.sendto(b'==========', ('127.0.0.1', 12345))
+        while True:  # Transmission loop
+            data = fp.read(PACKET_SIZE)
+            
+            if len(data) == 0:
+                break
+
+            sock.sendto(data, (RECEIVER_ADDR, RECEIVER_PORT))
+        
+        fp.close()
+        sock.sendto(b'==========', (RECEIVER_ADDR, RECEIVER_PORT))
