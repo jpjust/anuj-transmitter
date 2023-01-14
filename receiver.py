d238c530621c2d66f628ee3f513ae26c78b80935
João Paulo Just Peixoto <just1982@gmail.com>
2023-01-14 19:02:16 +0000

    Make receiver loop forever and timestamp the file

--- receiver.py
+++ receiver.py
@@ -1,18 +1,24 @@
 from socket import *
+from datetime import datetime
+
+BIND_ADDR = '0.0.0.0'
+BIND_PORT = 12345
+FILE_PREFIX = 'photo'
+PACKET_SIZE = 4096
 
 if __name__ == '__main__':
     sock = socket(family=AF_INET, type=SOCK_DGRAM)
-    sock.bind(('0.0.0.0', 12345))
-
-    fp = open('received.jpg', 'wb')
+    sock.bind((BIND_ADDR, BIND_PORT))
+    fp = None
 
     while True:
-        data = sock.recv(4096)
+        if fp == None:
+            now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+            fp = open(f"{FILE_PREFIX}_{now}.jpg", 'wb')
+        
+        data = sock.recv(PACKET_SIZE)
 
         if data == b'==========':
             fp.close()
-            exit(0)
-            
-        fp.write(data)
-        print('> ', end='')
-        print(data)
+        else:
+            fp.write(data)
